df9de2f0bb6b3316609d9db0c6d859aa7ae2e366
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-14 16:18:47 +0530

    setting mongodb cluster

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-ansible-playbook -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=rabbitmq &>>/opt/userdata.log
