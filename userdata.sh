cc2b472122cfdb969d079e7479faa0c96a101096
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-21 19:30:07 +0530

    setting up load balancer

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
 pip3.11 install botocore boto3 &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/sivalingam2/ansible_project.git main.yml -e component=rabbitmq -e env=${env} &>>/opt/userdata.log
